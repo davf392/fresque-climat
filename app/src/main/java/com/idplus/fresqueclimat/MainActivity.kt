0209117de6cb0d42ca8c48bc5b48c3292cded633
David Fourdrigniez <davidfourdrigniez@gmail.com>
2022-12-21 00:20:28 +0100

    replace tab layout by bottom navigation view (material)

--- app/src/main/java/com/idplus/fresqueclimat/MainActivity.kt
+++ app/src/main/java/com/idplus/fresqueclimat/MainActivity.kt
@@ -1,50 +1,43 @@
 package com.idplus.fresqueclimat
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.tabs.TabItem
-import com.google.android.material.tabs.TabLayout
-import com.idplus.fresqueclimat.adapter.FreskFragmentAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.idplus.fresqueclimat.adapter.FreskViewPagerAdapter
+import com.idplus.fresqueclimat.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var tabLayout: TabLayout
-    lateinit var viewPager: ViewPager
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // display the app bar from the layout
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        tabLayout = findViewById(R.id.tab_layout)
-        viewPager = findViewById(R.id.view_pager)
-
-
-        val adapter = FreskFragmentAdapter(this, supportFragmentManager)
-        viewPager!!.adapter = adapter
-
-        tabLayout!!.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                viewPager!!.currentItem = tab!!.position
+        val listOfFragments = listOf(NewsFragment(), SessionFragment(), ResourcesFragment())
+        binding.mainViewPager.adapter = FreskViewPagerAdapter(this, listOfFragments)
+        binding.mainViewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                when(position) {
+                    0 -> binding.bottomNavigationView.menu.findItem(R.id.miNews).isChecked = true
+                    1 -> binding.bottomNavigationView.menu.findItem(R.id.miSessions).isChecked = true
+                    2 -> binding.bottomNavigationView.menu.findItem(R.id.miResources).isChecked = true
+                }
             }
-            override fun onTabUnselected(tab: TabLayout.Tab?) { }
-            override fun onTabReselected(tab: TabLayout.Tab?) { }
         })
 
-        tabLayout!!.setupWithViewPager(viewPager)
-
-        // creating tab icons
-        tabLayout!!.getTabAt(0)?.icon = resources.getDrawable(R.drawable.ic_baseline_newspaper_24)
-        tabLayout!!.getTabAt(1)?.icon = resources.getDrawable(R.drawable.ic_baseline_groups_24)
-        tabLayout!!.getTabAt(2)?.icon = resources.getDrawable(R.drawable.ic_baseline_explore_24)
+        // Listen bottom navigation tabs change
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.miNews -> binding.mainViewPager.setCurrentItem(0, true)
+                R.id.miSessions -> binding.mainViewPager.setCurrentItem(1, true)
+                R.id.miResources -> binding.mainViewPager.setCurrentItem(2, true)
+            }
+            true
+        }
     }
 }
--- app/src/main/java/com/idplus/fresqueclimat/adapter/FreskFragmentAdapter.kt
+++ app/src/main/java/com/idplus/fresqueclimat/adapter/FreskFragmentAdapter.kt
@@ -1,59 +1,17 @@
 package com.idplus.fresqueclimat.adapter
 
-import android.content.Context
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import com.idplus.fresqueclimat.NewsFragment
-import com.idplus.fresqueclimat.ResourcesFragment
-import com.idplus.fresqueclimat.SessionFragment
-
-class FreskFragmentAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
 
-    val TAB_ITEM_NEWS = 0
-    val TAB_ITEM_SESSIONS = 1
-    val TAB_ITEM_RESOURCES = 2
-    val TAB_ITEM_ANTISECHE = 3
-    val TAB_ITEM_QUIZZ = 4
+class FreskViewPagerAdapter(fm: FragmentActivity, private val fragments: List<Fragment>) : FragmentStateAdapter(fm) {
 
-    val TOTAL_TABS = 3
-
-    val TAB_LABEL_NEWS: String = "ACTUALITES"
-    val TAB_LABEL_SESSIONS: String = "SESSIONS"
-    val TAB_LABEL_RESOURCES: String = "RESSOURCES"
-    val TAB_LABEL_ANTISECHE: String = "ANTISECHE"
-    val TAB_LABEL_QUIZZ: String = "QUIZZ"
-
-
-
-    override fun getCount(): Int {
-        return TOTAL_TABS
+    override fun getItemCount(): Int {
+        return fragments.size
     }
 
-    override fun getItem(position: Int): Fragment {
-        when (position) {
-            TAB_ITEM_NEWS       -> { return NewsFragment() }
-            TAB_ITEM_SESSIONS   -> { return SessionFragment() }
-            TAB_ITEM_RESOURCES  -> { return ResourcesFragment() }
-//            TAB_ITEM_ANTISECHE  -> { return ResourcesFragment() }
-//            TAB_ITEM_QUIZZ  -> { return ResourcesFragment() }
-            else -> {
-                return NewsFragment()
-            }
-        }
-    }
-
-    override fun getPageTitle(position: Int): CharSequence {
-        return when (position) {
-            TAB_ITEM_NEWS -> TAB_LABEL_NEWS
-            TAB_ITEM_SESSIONS -> TAB_LABEL_SESSIONS
-            TAB_ITEM_RESOURCES -> TAB_LABEL_RESOURCES
-//            TAB_ITEM_ANTISECHE -> TAB_LABEL_RESOURCES
-//            TAB_ITEM_QUIZZ -> TAB_LABEL_RESOURCES
-            else -> {
-                return TAB_LABEL_NEWS
-            }
-        }
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
     }
 }
